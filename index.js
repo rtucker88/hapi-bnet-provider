ecdccced6dad4351a5b3f9238bdf27768587b875
Ryan Tucker <rtucker88@gmail.com>
2014-11-12 15:35:57 -0500

    Initial commit of code

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ var bnet = require('./provider/bnet');
 var internals = {};
 
 exports.register = function(plugin, options, next) {
-    plugin.dependency('bell', function(plugin, next) {
-        var bell = require('bell');
-        console.log('bell providers: ', bell.providers);
-        bell.providers.bnet = bnet;
-        // Register the bnet provider with the built-ins
-        next();
-    });
+    var bell = require('bell');
+    bell.providers.bnet = bnet;
+    plugin.dependency('bell');
     next();
 };
 
